65b3b7933ff456d7f3a61a5ddacb401006580927
Fernando Santos <fnandojrs@hotmail.com>
2026-05-30 02:16:45 -0300

    Primeira versão da API

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pool = mysql.createPool({
+  host: 'yamabiko.proxy.rlwy.net',
+  user: 'root',
+  password: 'llfblPKemmWeXTdNLPnpoCWKASoDrWRO',
+  database: 'railway',
+  port: 30057
+});
+
+app.post('/fornecedor', async (req, res) => {
+  try {
+
+    const {
+      empresa,
+      cnpj,
+      responsavel,
+      telefone,
+      email,
+      produto,
+      observacao,
+      rua,
+      bairro,
+      numero,
+      cep,
+      complemento
+    } = req.body;
+
+    await pool.query(`
+      INSERT INTO fornecedor (
+        empresa,
+        cnpj,
+        responsavel,
+        telefone,
+        email,
+        produto,
+        observacao,
+        rua,
+        bairro,
+        numero,
+        cep,
+        complemento
+      )
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `, [
+      empresa,
+      cnpj,
+      responsavel,
+      telefone,
+      email,
+      produto,
+      observacao,
+      rua,
+      bairro,
+      numero,
+      cep,
+      complemento
+    ]);
+
+    res.json({ sucesso: true });
+
+  } catch (erro) {
+
+    console.error(erro);
+    res.status(500).json({ erro: erro.message });
+
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API rodando na porta ${PORT}`);
+});
